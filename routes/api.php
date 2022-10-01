631949e9ead726661292785f5a5d60ec2ddf1c2a
Yan Gonçalves <yanw100@gmail.com>
2022-10-01 01:38:42 -0300

    [Feature] CRUD routes for lists

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\Auth\AuthenticationController;
+use App\Http\Controllers\Lists\ListsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,7 +23,17 @@ Route::get('/', [IndexController::class, 'index']);
 Route::post('signup', [AuthenticationController::class, 'signup']);
 Route::post('signin', [AuthenticationController::class, 'signin']);
 
-Route::group(['middleware' => 'auth:sanctum'], function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('signout', [AuthenticationController::class, 'signout']);
     Route::get('welcome', [IndexController::class, 'greetings']);
+
+    Route::prefix('lists')->group(function () {
+        Route::controller(ListsController::class)->group(function () {
+           Route::get('/', 'index');
+           Route::get('/{list_id}', 'show');
+           Route::post('/', 'store');
+           Route::put('/{list_id}', 'update');
+           Route::delete('/{list_id}', 'destroy');
+        });
+    });
 });
